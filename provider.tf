bb9667fee22b901e437c39e8f1f3e11ac839b0fe
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-24 12:58:27 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
